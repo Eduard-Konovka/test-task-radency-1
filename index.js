728e7372807d5817922b25ff63f05d80adbc9ba2
Eduard-Konovka <ed098ua@gmail.com>
2022-03-31 16:41:22 +0300

    Adds start files

---
+++ index.js
@@ -0,0 +1,7 @@
+const chooseOptimalDistance = (t, k, ls) => {
+  // твій код
+  return null;
+};
+
+chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]); //173
+chooseOptimalDistance(163, 3, [50]); // null
