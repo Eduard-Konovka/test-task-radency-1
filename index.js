ac6f563c93a76ea1cd06d823579b23513464ebcd
Eduard-Konovka <ed098ua@gmail.com>
2022-04-02 19:05:35 +0300

    Adds a job execution

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const chooseOptimalDistance = (t, k, ls) => {
-  // твій код
-  return null;
+  if (ls.length < k) {
+    return null;
+  }
+
+  const amounts = [0];
+
+  for (let a = 0; a < k; a++) {
+    const acc = [...amounts];
+
+    for (let i = 0; i < amounts.length; i++) {
+      for (let j = 0; j < ls.length; j++) {
+        acc.push(amounts[i] + ls[j]);
+      }
+    }
+
+    amounts.push(...acc);
+  }
+
+  const result = amounts.filter((el) => el < t);
+
+  return result[result.length - 1];
 };
 
-chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]); //173
-chooseOptimalDistance(163, 3, [50]); // null
+console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61]));
+console.log(chooseOptimalDistance(163, 3, [50]));
